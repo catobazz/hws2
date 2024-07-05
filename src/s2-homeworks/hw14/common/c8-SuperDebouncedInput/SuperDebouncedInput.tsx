7f3194047cf9937324a00606f21573a39f8fd86a
Artem Rybin <catobazzz@gmail.com>
2024-07-05 11:36:09 +0300

    HW14 feat: Add debounce functionality to SuperDebouncedInput component
         - Implement debounced onChange callback to delay function execution by 1500ms
         - Clear previous timeout before setting a new one to ensure correct debouncing

--- src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
+++ src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
@@ -26,13 +26,19 @@ const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps
     }
 ) => {
-    const [timerId, setTimerId] = useState<number | undefined>(undefined)
+    const [timerId, setTimerId] = useState<NodeJS.Timeout | undefined>(undefined)
 
     const onChangeTextCallback = (value: string) => {
         onChangeText?.(value)
 
         if (onDebouncedChange) {
-            // делает студент
+            if (timerId) {
+                clearTimeout(timerId)
+            }
+            const newTimeout = setTimeout(() => {
+                onDebouncedChange(value)
+            }, 1500)
+            setTimerId(newTimeout)
 
             // остановить предыдущий таймер
             // запустить новый на 1500ms, в котором вызовется функция
